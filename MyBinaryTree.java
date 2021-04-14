817e5751abc65be739972729b396541a1981e758
akilds <akilrainadesigan@gmail.com>
2021-04-14 11:13:42 +0530

    ADDED : Search in Binary Tree

--- MyBinaryTree.java
+++ MyBinaryTree.java
@@ -2,6 +2,7 @@ public class MyBinaryTree<K extends Comparable<K>> {
 
 	private MyBinaryNode<K> root;
 
+   //USE CSAE 1, 2 - Creation of Binary Tree
 	public void add(K key)
 	{
 		this.root = this.addRecursively(root, key);
@@ -33,6 +34,34 @@ public class MyBinaryTree<K extends Comparable<K>> {
 	{
 		return current == null ? 0 : 1 + this.getSizeRecursive(current.left)
 		                               + this.getSizeRecursive(current.right);
+	}
+
+   //USE CASE 3 - Search in Binary Tree
+   public boolean search(K key)
+	{
+		if(this.root == key)
+		{
+			return true;
+		}
+		else
+		{
+			return searchRecursively(root, key);
+		}
+	}
+
+	private boolean searchRecursively(MyBinaryNode<K> current, K key)
+	{
+		int compareResult = key.compareTo(current.key);
+	    if(compareResult == 0) return true;
+	    if(compareResult < 0)
+	    {
+	    	searchRecursively(current.left, key);
+	    }
+	    else
+	    {
+	    	searchRecursively(current.right, key);
+	    }
+	    return true;
 	}
 }
 
