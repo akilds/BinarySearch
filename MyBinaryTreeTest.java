9a3bd7354fde9dad2a22b2ae411b26784ffda733
akilds <akilrainadesigan@gmail.com>
2021-04-14 10:40:46 +0530

    ADDED : Creation of Binary Tree

--- MyBinaryTreeTest.java
+++ MyBinaryTreeTest.java
@@ -5,7 +5,7 @@ public class MyBinaryTreeTest {
 
    //TEST CASE 1
 	@Test
-	public void treeSearchChech()
+	public void treeSearchCheck()
 	{
 		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
 		myBinaryTree.add(56);
@@ -13,5 +13,27 @@ public class MyBinaryTreeTest {
 		myBinaryTree.add(70);
 		int size = myBinaryTree.getSize();
 		Assert.assertEquals(3, size);
+	}
+
+   //TEST CASE 2
+   @Test
+	public void treeSearchCheck1()
+	{
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		myBinaryTree.add(22);
+		myBinaryTree.add(40);
+		myBinaryTree.add(60);
+		myBinaryTree.add(95);
+		myBinaryTree.add(11);
+		myBinaryTree.add(65);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);
+		int size = myBinaryTree.getSize();
+		Assert.assertEquals(13, size);
 	}
 }
